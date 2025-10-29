e5df67d4b4c85b202e1b3cb3d657e9e0bd58fb28
seo-sang-ho <rocter2049@naver.com>
2025-10-29 15:25:04 +0900

    add findByCategory

--- src/main/java/com/example/mapmemo/controller/MainController.java
+++ src/main/java/com/example/mapmemo/controller/MainController.java
@@ -1,18 +1,29 @@
 package com.example.mapmemo.controller;
 
+import com.example.mapmemo.entity.Memo;
+import com.example.mapmemo.service.MemoService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
     @Value("${kakao.api.key}")
     private String kakaoApikey;
+    private final MemoService memoService;
+
+    public MainController(MemoService memoService) {
+        this.memoService = memoService;
+    }
 
     @GetMapping("/")
     public String index(Model model){
+        List<Memo> memos = memoService.getAllMemos();
+        model.addAttribute("memos",memos);
         model.addAttribute("kakaoApikey", kakaoApikey);
         return "index";
     }
