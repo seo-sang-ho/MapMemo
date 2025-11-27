b8b14f17ffd6bdf57bc94c53c2242dd8d0e21cc0
seo-sang-ho <rocter2049@naver.com>
2025-11-27 18:32:10 +0900

    비밀번호 암호화

---
+++ src/main/java/com/example/mapmemo/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.example.mapmemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder
+}
