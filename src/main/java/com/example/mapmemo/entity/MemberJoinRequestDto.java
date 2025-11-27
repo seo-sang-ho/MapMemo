e02a370fb06383239eccf8fc3ad8ca9687c0807e
seo-sang-ho <rocter2049@naver.com>
2025-11-27 18:29:11 +0900

    회원 가입 DTO

--- src/main/java/com/example/mapmemo/entity/MemberJoinRequestDto.java
+++ src/main/java/com/example/mapmemo/entity/MemberJoinRequestDto.java
@@ -1,4 +1,12 @@
 package com.example.mapmemo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class MemberJoinRequestDto {
+    private String loginId;
+    private String password;
+    private String email;
+    private String name;
 }
