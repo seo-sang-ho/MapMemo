b1e8038c1c5f9cf9ffa3f97ba4d80f8789136a89
seo-sang-ho <rocter2049@naver.com>
2025-11-27 22:25:04 +0900

    JWT 설정

--- src/main/java/com/example/mapmemo/config/JwtUtil.java
+++ src/main/java/com/example/mapmemo/config/JwtUtil.java
@@ -1,4 +1,52 @@
-package com.example.mapmemo.config;
+package com.example.mapmemo.security;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret-key}")
+    private String SECRET_KEY;
+    private final long EXPIRATION_MS = 1000 * 60 * 60; // 1시간
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
 }
