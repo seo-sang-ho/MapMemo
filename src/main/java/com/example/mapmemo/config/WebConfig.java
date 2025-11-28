1008eda0dfe05a843a43eeba97261a39836792d0
seo-sang-ho <rocter2049@naver.com>
2025-11-28 17:50:06 +0900

    SecurityConfig에 다시 작성했기때문에 필요X

--- src/main/java/com/example/mapmemo/config/WebConfig.java
+++ src/main/java/com/example/mapmemo/config/WebConfig.java
@@ -1,30 +1,30 @@
-package com.example.mapmemo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowCredentials(true);
-            }
-
-            // spa 포워딩
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addViewController("/{path:[^\\.]*}")
-                        .setViewName("forward:/index.html");
-            }
-        };
-    }
-}
+//package com.example.mapmemo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig {
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer(){
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/api/**")
+//                        .allowedOrigins("http://localhost:5173")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowCredentials(true);
+//            }
+//
+//            // spa 포워딩
+//            @Override
+//            public void addViewControllers(ViewControllerRegistry registry) {
+//                registry.addViewController("/{path:[^\\.]*}")
+//                        .setViewName("forward:/index.html");
+//            }
+//        };
+//    }
+//}
