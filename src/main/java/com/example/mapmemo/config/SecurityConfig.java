7225040d5e962315bc25fdee82f9dcb509c5ce19
seo-sang-ho <rocter2049@naver.com>
2026-06-09 14:55:38 +0900

    deploy test

--- src/main/java/com/example/mapmemo/config/SecurityConfig.java
+++ src/main/java/com/example/mapmemo/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/signup").permitAll()
                         .requestMatchers("/api/admin/toilets/**").permitAll()
 
                         .requestMatchers("/api/toilets").permitAll()
