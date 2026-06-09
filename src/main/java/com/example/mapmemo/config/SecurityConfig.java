47bddcf1d9f9f48aa1c17f77aa8db8033febd7c2
seo-sang-ho <rocter2049@naver.com>
2026-06-09 14:53:41 +0900

    403 Error fix return

--- src/main/java/com/example/mapmemo/config/SecurityConfig.java
+++ src/main/java/com/example/mapmemo/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/admin/toilets/**").permitAll()
 
                         .requestMatchers("/api/toilets").permitAll()
